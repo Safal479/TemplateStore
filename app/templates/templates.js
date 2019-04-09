22d60eac4e5ba747c95563bbf374af606937ee45
Safal Bastola <bastolasafal479@gmail.com>
2019-04-09 13:51:31 +0545

    Json Extracted

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,14 +1,38 @@
 'use strict';
-angular.module('templateStore.templates', ['ngRoute'])
+angular.module('templateStore.templates', ['ngRoute','angular.filter'])
 
-.config(['$routeProvider', function($routeProvider){
-    $routeProvider.
-        when('/templates', {
-            templateUrl: 'templates/templates.html',
-            controller: 'TemplatesCtrl'
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/templates', {
+                templateUrl: 'templates/templates.html',
+                controller: 'TemplatesCtrl'
+            })
+            .when('/templates/:templateId', {
+                templateUrl: 'templates/templates-details.html',
+                controller: 'TemplateDetailsCtrl'
+            });
+    }])
+
+    .controller('TemplatesCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http({
+            method: 'GET',
+            url: '/json/templates.json'
+        }).then(function (response) {
+            $scope.data = response.data;
         });
-}])
-
-.controller('TemplatesCtrl', ['$scope', function($scope){
-
-}]);
+    }])
+    .controller('TemplateDetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', function ($scope, $http, $routeParams, $filter) {
+        var templateId = $routeParams.templateId;
+        $http({
+            method: 'GET',
+            url: '/json/templates.json'
+        }).then(function (response) {
+            $scope.template = $filter('filter')(response.data, function (d) {
+                return d.id == templateId;
+            })[0];
+            $scope.mainImage = $scope.template.images.name;
+        });
+        $scope.setImage = function (image) {
+            $scope.mainImage = image;
+        };
+    }]);
---
+++ app/templates/templates.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('templateStore.templates module', function() {
+
+  beforeEach(module('templateStore.templates'));
+
+  describe('Templates controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var TemplatesCtrl = $controller('TemplatesCtrl');
+      expect(TemplatesCtrl).toBeDefined();
+    }));
+
+  });
+});
